31e18a893fa299223aeef1141c63d03758548f76
marcelogdomingues <marcelo.domingues.ul@gmail.com>
2020-10-28 17:41:00 +0000

    Web Server Working

--- src/org/academiadecodigo/bootcamp/webserver/WebServer.java
+++ src/org/academiadecodigo/bootcamp/webserver/WebServer.java
@@ -1,11 +1,11 @@
 package org.academiadecodigo.bootcamp.webserver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
@@ -13,9 +13,9 @@ import java.util.logging.SimpleFormatter;
 public class WebServer {
 
     //Resource path
-    private static String path;
+    private static String path = "src/org/academiadecodigo/bootcamp/webserver/resources/index.html";
     //Socket
-    private static Socket socket;
+    private static Socket clientSocket;
     private static ServerSocket serverSocket;
     //Logger
     private static Logger logger = Logger.getLogger("ErrorLog");
@@ -23,32 +23,93 @@ public class WebServer {
     private static FileHandler fileHandler;
     private static BufferedReader request;
 
+    private static File file;
+    private static PrintWriter output;
+
+    public static File getFile() {
+        return file;
+    }
+
+    public static void setClientSocket(Socket clientSocket) {
+        WebServer.clientSocket = clientSocket;
+    }
 
     public static String getPath() {
         return path;
     }
 
-    public static void doRequest() {
-
+    public static Socket getSocket() {
+        return clientSocket;
     }
 
     public static void sendRequest(BufferedReader request) {
 
         try {
 
-            String getRequest = request.readLine();
-            String[] reqArray = getRequest.split(" ");
+            logger.info("Sending Request...");
 
+            String getRequest = request.readLine();
+
+            logger.info(getRequest);
 
         } catch (Exception e) {
 
-            logger.warning(e.getMessage());
+            logger.warning("Error in Send Request: " + e.getMessage());
 
         }
 
     }
 
-    public static void do
+    public static void doResponse() {
+
+        try {
+
+            logger.info("Performing Response ...");
+
+            output = new PrintWriter(getSocket().getOutputStream());
+            file = new File(getPath());
+
+        } catch (Exception e) {
+
+            logger.severe(e.getLocalizedMessage());
+
+        }
+    }
+
+    public static void sendResponse() {
+
+        doResponse();
+
+        try {
+
+            if (!file.exists()) {
+
+                output.write("HTTP/1.1 404 Not Found\r\n" + "Content-Type: text/html" + "\r\n\r\n");
+                logger.info("File does not exist.  Client requesting file at : " + getPath());
+                output.close();
+
+            } else {
+
+                output.write("HTTP/1.1 200 OK\r\n" + "Content-Type: text/html" + "\r\n\r\n");
+                logger.info(output.toString());
+                byte[] fileBytes = Files.readAllBytes(Paths.get(getPath()));
+                String fileString = new String(fileBytes, StandardCharsets.UTF_8);
+                output.write(fileString);
+
+                logger.info("Finished writing content to output");
+
+                output.close();
+
+            }
+
+        } catch (Exception e) {
+
+            logger.warning("Error in Send Response: " + e.getMessage());
+
+        }
+
+
+    }
 
     public static BufferedReader getRequest() {
         return request;
@@ -59,7 +120,9 @@ public class WebServer {
         // Accept new client connection
         try {
 
-            Socket clientSocket = serverSocket.accept();
+            logger.info("Reading Request...");
+
+            clientSocket = serverSocket.accept();
 
             InputStream inputStream = clientSocket.getInputStream();
 
@@ -68,37 +131,35 @@ public class WebServer {
 
         } catch (Exception e) {
 
-            logger.warning(e.getMessage());
+            logger.warning("Error in Read Request: " + e.getMessage());
 
         }
 
     }
 
-    public static void responseWaiter() {
-
-    }
 
     public static void setupServer() {
 
         try {
 
-            logger.info("Setup Server");
+            logger.info("Setting Up The Server...");
 
             int port = 8080;
 
             serverSocket = new ServerSocket(port);
 
-            System.out.println("Server Started and listening to the port " + port);
             logger.info("Server Started and listening to the port " + port);
 
             readRequest(serverSocket);
 
             sendRequest(getRequest());
 
+            sendResponse();
+
         } catch (Exception e) {
 
             logger.warning(e.getMessage());
-            System.out.println("Error: " + e.getMessage());
+            System.out.println("Error in Setup Server: " + e.getMessage());
 
         }
 
@@ -116,10 +177,6 @@ public class WebServer {
             logger.info("Error Log");
 
 
-        } catch (SecurityException e) {
-
-            e.printStackTrace();
-
         } catch (IOException e) {
 
             e.printStackTrace();
@@ -128,10 +185,9 @@ public class WebServer {
 
     }
 
-
     public static void start() {
 
-        setupServer();
+            setupServer();
 
     }
 
